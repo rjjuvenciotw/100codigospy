753e53c9ef0d8b950b29deeef10a32f65fa8095d
Ricardo Miguel <86057517+rjjuvenciotw@users.noreply.github.com>
2025-03-26 19:59:09 -0300

    Create 02.py

---
+++ 02.py
@@ -0,0 +1,9 @@
+#Crie um programa que peça dois números
+# inteiros ao usuário e exiba a soma deles.
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite um segundo número: "))
+
+resultado = num1 + num2
+
+print(f"{num1} + {num2} = {resultado}")
