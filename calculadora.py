57ec93b1d575412093a65eeae16fdb64a30f5db8
Ricardo Miguel <86057517+rjjuvenciotw@users.noreply.github.com>
2025-03-28 18:56:58 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,61 +1,61 @@
-
 import math 
+from rich import print
 
-numero0 = float(input("Digite um primeiro número: "))
-numero1 = float(input("Digite um segundo número: "))
+while True:
+    def menu():
+        print("[bold red] \n------- CALCULADORA --------[/bold red]")
+        print("1 - Somar")
+        print("2 - Subtrair")
+        print("3 - Multiplicar")
+        print("4 - Dividir")
+        print("5 - Potência")
+        print("6 - Raiz quadrada")
 
-def menu(): 
+        opcao = input("Escolha uma opção: ")
 
-    print("/n-------Calculadora--------")
-    print("1 - para somar")
-    print("2 - para subtrair")
-    print("3 - para multiplicar")
-    print("4 - para dividir")
-    print("5 - para potência")
-    print("6 - para raíz quadrada")
+        if opcao in ["1", "2", "3", "4", "5"]:
+            numero0 = float(input("Digite o primeiro número: "))
+            numero1 = float(input("Digite o segundo número: "))
 
-    opcao = input("Escolah uma opção: ")
+        if opcao == "1":
+            somar(numero0, numero1)
+        elif opcao == "2":
+            subtrair(numero0, numero1)
+        elif opcao == "3":
+            multiplicar(numero0, numero1)
+        elif opcao == "4":
+            dividir(numero0, numero1)
+        elif opcao == "5":
+            potencia(numero0, numero1)
+        elif opcao =="6":
+            raiz(numero0, numero1)
 
-    if opcao == "1":
-        somar()
-    elif opcao == "2":
-        subtrair()
-    elif opcao == "3":
-        multiplicar()
-    elif opcao == "4":
-        dividir()
-    elif opcao == "5":
-        potencia()
-    elif opcao =="6":
-        raiz()
+        else:
+            print("ERRO : Digite uma opção válida!")
 
-    else:
-        print("ERRO : Digite uma opção válida!")
+    def somar(numero0, numero1):
+        resultado = numero0 + numero1
+        print(f"A soma dos números{numero0} e {numero1} é de: {resultado}")    
+        
+    def subtrair(numero0, numero1):
+        resultado = numero0 - numero1 
+        print(f"A subtração dos números {numero0} e {numero1} é de: {resultado}")
 
-def somar(numero0, numero1):
-    resultado = numero0 + numero1
-    print(f"A soma dos números{numero0} e {numero1} é de: {resultado}")    
-    
-def subtrair(numero0, numero1):
-    resultado = numero0 - numero1 
-    print(f"A subtração dos números {numero0} e {numero1} é de: {resultado}")
+    def multiplicar(numero0, numero1):
+        resultado = numero0 * numero1 
+        print(f"A multiplicação dos números {numero0} e {numero1} é de: {resultado}")
 
-def multiplicar(numero0, numero1):
-    resultado = numero0 * numero1 
-    print(f"A multiplicação dos números {numero0} e {numero1} é de: {resultado}")
+    def dividir(numero0, numero1):
+        resultado = numero0 / numero1
+        print(f"A divisão dos números {numero0} e {numero1} é de {resultado}")
 
-def dividir(numero0, numero1):
-    resultado = numero0 / numero1
-    print(f"A divisão dos números {numero0} e {numero1} é de {resultado}")
+    def potencia(numero0, numero1):
+        resultado = numero0 ** numero1
+        print(f"Sendo {numero0} a base, e {numero1} o expoente, o resultado da pontecialização é de {resultado}.")
 
-def potencia(numero0, numero1):
-    resultado = numero0 ** numero1
-    print("Sendo {numero0} a base, e {numero1} o expoente, o resultado da pontecialização é de {resultado}.")
+    def raiz(numero0, numero1):
+        raiz_cubica0_math = math.sqrt(numero0)
+        raiz_cubica1_math = math.sqrt(numero1)
+        print(f"{raiz_cubica0_math} e {raiz_cubica1_math} ")
 
-def raiz(numero0, numero1):
-    raiz_cubica0_math = math.sqrt(numero0)
-    raiz_cubica1_math = math.sqrt(numero1)
-    print("{raiz_cubica0_math} e {raiz_cubica1_math} ")
-
-menu()
-
+    menu()
